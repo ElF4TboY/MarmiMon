73b6a9b7cdbaafbbd32f7f1fb01cfcc348653ccf
ElF4TboY <thomasmarchand63@gmail.com>
2023-07-16 11:23:48 +0200

    added search functionality

---
+++ src/function/selectRecipe.js
@@ -0,0 +1,12 @@
+const selectRecipe = (recipes, id, value) => {
+  for (let recipe of recipes) {
+    if (recipe.id.includes(id) || recipe.ingredients.includes(value)) {
+      list.classList.add("checked");
+      list.innerHTML += `<h2>${recipe.name}</h2>
+                                  <li>${recipe.ingredients}</li>
+                                  <p>${recipe.description}</p>`;
+    }
+  }
+};
+
+export { selectRecipe };

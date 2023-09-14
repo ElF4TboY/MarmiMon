2ff7ddf29fb4a9aeb9162f38b8ff8cebd138e077
ElF4TboY <thomasmarchand63@gmail.com>
2023-09-14 10:00:03 +0200

    refactoring and deleting function

--- src/function/selectRecipe.js
+++ src/function/selectRecipe.js
@@ -1,6 +1,6 @@
 const selectRecipe = (recipes, id, value) => {
   for (let recipe of recipes) {
-    if (recipe.id.includes(id) || recipe.ingredients.includes(value)) {
+    if (recipe.classId.includes(id) || recipe.ingredients.includes(value)) {
       list.classList.add("checked");
       list.innerHTML += `<h2>${recipe.name}</h2>
                                   <li>${recipe.ingredients}</li>

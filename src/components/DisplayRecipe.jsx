ed07fc7dbedca2db7684b34158d815ba232e6acd
ElF4TboY <thomasmarchand63@gmail.com>
2023-09-19 23:56:42 +0200

    e

--- src/components/DisplayRecipe.jsx
+++ src/components/DisplayRecipe.jsx
@@ -2,6 +2,7 @@ export const DisplayRecipe = ({ recipe }) => {
   return (
     <div className="recipe-container">
       <h2>{recipe.name}</h2>
+      <p>glop</p>
       <ul>{recipe.ingredients}</ul>
       <p>{recipe.description}</p>
     </div>
